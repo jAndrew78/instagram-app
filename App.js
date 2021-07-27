1fad47f36a7c7b00d385ed7c7f53a19447d1a0e4
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-07-27 13:46:00 -0500

    firebase setup

--- App.js
+++ App.js
@@ -3,25 +3,28 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import { FirebaseProvider } from './src/context/FirebaseContext';
 import { LandingScreen, RegisterScreen } from './src/components';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRoutName="Landing">
-        <Stack.Screen 
-          name="Landing" 
-          component={LandingScreen} 
-          options={{headerShown: false}}
-        />
-        <Stack.Screen 
-          name="Register" 
-          component={RegisterScreen}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <FirebaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRoutName="Landing">
+          <Stack.Screen 
+            name="Landing" 
+            component={LandingScreen} 
+            options={{headerShown: false}}
+          />
+          <Stack.Screen 
+            name="Register" 
+            component={RegisterScreen}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </FirebaseProvider>
   );
 }
---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCg-IteRlLVy_WNpXbdzLrvCyEt3Bl6zb4",
+  authDomain: "instagram-app-16.firebaseapp.com",
+  projectId: "instagram-app-16",
+  storageBucket: "instagram-app-16.appspot.com",
+  messagingSenderId: "697273472531",
+  appId: "1:697273472531:web:0054b7c4fd8752268851e6"
+};
